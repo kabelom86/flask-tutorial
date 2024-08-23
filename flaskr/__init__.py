9509d4ee3d3c80de864bb295bd52d0d570e920b4
Kabelo Peele <kabelom.p@live.com>
2024-08-23 14:29:14 +0200

    Created 'auth.py' file to handle all registering and login/out details. Updated '__init__.py' file with 'register' blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -32,4 +32,8 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    # Registering an authentication view
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     return app
