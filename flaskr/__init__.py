6e83a194ed5bafd2287a6e67d182a4bfccabf58d
Kabelo Peele <kabelom.p@live.com>
2024-08-23 16:27:49 +0200

    Created blog dir with html files in 'templates' and 'blog.py' view file

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -36,4 +36,9 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    # Registering the blog view
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
